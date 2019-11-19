d9894ad8a51517899f5a408a41a9b288fa3949fe
Justin <justin_bertrand@yahoo.com>
2019-11-19 10:47:57 -0600

    Added filters for city, state, country, and shape

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,88 +1,83 @@
-// from data.js
-var tableData = data;
-
-// Get a reference to the table body
+// Get a reference to the table body and the input button element
 var tbody = d3.select("tbody");
+var inputButton = d3.select("#filter-btn");
 
 // Console.log the UFO sighting data from data.js
 console.log(data);
 
-// // Step 1: Loop Through `data` and console.log each UFO sighting report object
-// // Step 2:  Use d3 to append one table row `tr` for each UFO sighting report object
-// // Step 3:  Use `Object.entries` to console.log each UFO sighting report value
-// // Step 4: Use d3 to append 1 cell per UFO sighting report value (Date, City, State, Country, Shape, Duration, Comments)
-// // Step 5: Use d3 to update each cell's text with
-// // UFO sighting report values (Date, City, State, Country, Shape, Duration, Comments)
+// Step 1: Loop Through `data` and console.log each UFO sighting report object
+// Step 2:  Use d3 to append one table row `tr` for each UFO sighting report object
+// Step 3:  Use `Object.entries` to console.log each UFO sighting report value
+// Step 4: Use d3 to append 1 cell per UFO sighting report value (Date, City, State, Country, Shape, Duration, Comments)
+// Step 5: Use d3 to update each cell's text with UFO sighting report values (Date, City, State, Country, Shape, Duration, Comments)
 data.forEach(function(ufoReport) {
-  // console.log(ufoReport);
+  
   var row = tbody.append("tr");
+
   Object.entries(ufoReport).forEach(function([key, value]) {
-    // console.log(key, value);
-    // Append a cell to the row for each value
-    // in the UFO sighting report object
+    
     var cell = row.append("td");
+    
     cell.text(value);
   });
 });
 
-//////
-
-// Getting a reference to the input element on the page with the id property set to 'input-field'
-// var inputField = d3.select("#datetime");
-
-// Getting a reference to the input button element
-var inputButton = d3.select("#filter-btn");
-
-// This function is triggered when the button is clicked
+// Run this function when the button is clicked
 function handleClick() {
   
-  // Brag about it
-//   console.log("A button was clicked!");
-
-//   // We can use d3 to see the object that dispatched the event
-//   console.log("This is the object that handled the event:");
-//   console.log(d3.event.target);
-  
-  //////
-
   // Select the input element and get the raw HTML node
   // var inputField = d3.select("#datetime");
 
-  // Get the value property of the input element
-  // var inputValue = inputField.property("value");
+  // Then get the value property of the input element
+  // var datetimeInput = inputField.property("value");
   
   // Or do it in one line, since you won't call inputField elsewhere
-  inputValue = d3.select("#datetime").property("value");
-
+  datetimeInput = d3.select("#datetime").property("value");
+  cityInput = d3.select("#city").property("value");
+  stateInput = d3.select("#state").property("value");
+  countryInput = d3.select("#country").property("value");
+  shapeInput = d3.select("#shape").property("value");
+  
   // See what you've got
-  console.log(inputValue);
+  console.log(datetimeInput, cityInput, stateInput, countryInput, shapeInput)
 
-  // Filter your data by inputValue and assign it to a variable
-  var filteredData = data.filter(sighting => sighting.datetime === inputValue);
+  var filteredData = data;
 
-  // As above, so below
-//   data.filter(doFilter);
-
-//   function doFilter(sighting) {
-//       return sighting.datetime === inputValue;
-//   }
+  // Filter your data by datetimeInput and assign it to a variable
+  if (datetimeInput) {
+    var filteredData = filteredData.filter(sighting => sighting.datetime === datetimeInput);
+  }
+  // Repeat filter with values from other input fields, if present
+  if (cityInput) {
+    var filteredData = filteredData.filter(sighting => sighting.city === cityInput.toLowerCase());
+  }
+  if (stateInput) {
+    var filteredData = filteredData.filter(sighting => sighting.state === stateInput.toLowerCase());
+  }
+  if (countryInput) {
+    var filteredData = filteredData.filter(sighting => sighting.country === countryInput.toLowerCase());
+  }
+  if (shapeInput) {
+    var filteredData = filteredData.filter(sighting => sighting.shape === shapeInput.toLowerCase());
+  }
+            
 
   // Check on it
   console.log(filteredData);
 
+  // Clear the table
   d3.select("tbody").selectAll("tr").remove();
 
+  // Repopulate table with filtered data
   filteredData.forEach(function(filteredReport) {
-    // console.log(ufoReport);
     var row = tbody.append("tr");
     Object.entries(filteredReport).forEach(function([key, value]) {
-      // console.log(key, value);
-      // Append a cell to the row for each value
-      // in the UFO sighting report object
+      // Append a cell to the row for each value in the UFO sighting report object
       var cell = row.append("td");
       cell.text(value);
     });
   });
+
 }
 
 // We can use the `on` function in d3 to attach an event to the handler function
